bddb8956dfc66ad4c5209daa415b97b788d7d472
Max Kornilev <kornilev.maxim@mail.ru>
2025-03-13 11:13:38 +0300

    add logo-image

--- diplomproject.client/src/components/Header.tsx
+++ diplomproject.client/src/components/Header.tsx
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { redirectAndRemoveToken } from '../data/Functions';
 import { useNavigate } from 'react-router-dom';
 import '../styles/header.css';
+import logo from '../images/mstuca.png';
 
 const Header: React.FC = () => {
     const navigate = useNavigate();
@@ -16,7 +17,16 @@ const Header: React.FC = () => {
     return (
         <header className="bg-primary text-white py-4 fixed-top">
             <div className="container d-flex justify-content-between align-items-center">
-                <h4 className="mb-0 font-weight-bold text-white">Добро пожаловать в СНО и СМУ!</h4>
+                <div className="d-flex align-items-center">
+                    <div className="logo-background">
+                        <img
+                            src={logo}
+                            alt="Логотип"
+                            className="logo-image rounded-circle"
+                        />
+                    </div>
+                    <h4 className="mb-0 font-weight-bold text-white">Добро пожаловать в СНО и СМУ!</h4>
+                </div>
                 <button
                     className="btn btn-warning custom-btn"
                     onClick={handleLogout}
